df2ddb7f0d8f41b06edf1bdff1ca258c5e7558e8
pablosoon <pa.meliqueo@gmail.com>
2022-05-23 15:19:31 -0400

    visita admin base de datos

--- PrayTheNews/admin.py
+++ PrayTheNews/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 
-from PrayTheNews.models import Juego
+from PrayTheNews.models import Juego, Rol,Usuario,Status,TipoPubli,Publicacion,Comentario,Parrafo
 
 # Register your models here.
 
 admin.site.register(Juego)
+admin.site.register(Rol)
+admin.site.register(Usuario)
+admin.site.register(Status)
+admin.site.register(TipoPubli)
+admin.site.register(Publicacion)
+admin.site.register(Comentario)
+admin.site.register(Parrafo)
