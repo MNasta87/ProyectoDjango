6c30a97c14f8eefe5d9fafcc5f604550e84bd855
pablosonn <pa.meliqueo@duocuc.cl>
2022-06-01 23:23:40 -0400

    Deje la cagada con los link pero es un mal muy necesario Esta incompleto pero la estructura esta definida

--- PrayTheNews/urls.py
+++ PrayTheNews/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import EditarCuentaUsuartio, FormularioUsuarios, PerfilCompleto, PublicarAnalisis, RegistroAnalisis,RegistroNoticia,MenuNoticias, NoticiasIndividuales, NoticiaValorant,MenuPrincipal,Catalogo,MenuAnalisis,RegistroUsuarios,LoginUsuario,AdministrarUsuario,BuscarAnalisis,BuscarUsuario,EditarUsuario,CuentaUsuario,EditarCuenta,PerfilUsuario,PublicarNoticia
+from .views import CambiarContraAdmin, CuentaAdmin, CuentaPerio, EditarCuentaAdmin, EditarCuentaPerio, EditarCuentaU, FormularioUsuarios, GuardarCuentaAdmin, PerfilCompleto, PublicarAnalisis, RegistroAnalisis,RegistroNoticia,MenuNoticias, NoticiasIndividuales, NoticiaValorant,MenuPrincipal,Catalogo,MenuAnalisis,RegistroUsuarios,LoginUsuario,AdministrarUsuario,BuscarAnalisis,BuscarUsuario,EditarUsuario,CuentaUsuario,PerfilUsuario,PublicarNoticia 
                     
 
 
@@ -21,16 +21,31 @@ urlpatterns = [
     path('PublicarAnalisis/',PublicarAnalisis,name="PublicarAnalisis"),
     path('RegistroAnalisis/',RegistroAnalisis,name="RegistroAnalisis"),
 
-    path('Perfil/',EditarCuentaUsuartio, name= "EditarCuentaU"),
+    #Perfiles
+    #Admin
+    path('PerfilAdmin/',CuentaAdmin,name="CuentaAdmin"),
+    path('CambiarContraAdmin/',CambiarContraAdmin,name="CambiarContraAdmin"),
+    
+    path('EditarCuentaAdmin/',EditarCuentaAdmin,name="EditarCuentaAdmin"),
+    path('GuardarCuentaAdmin/',GuardarCuentaAdmin,name="GuardarCuentaAdmin"),
+
+    #Usuario
+    path('Perfil/',PerfilUsuario,name="PerfilUsuario"),
+    path('EditarCuenta/',EditarCuentaU,name="EditarCuentaU"),
+    path('CuentaUsuario/',CuentaUsuario,name="CuentaUsuario"),
+    #Periodista
+    path('PerfilPeriodista/',CuentaPerio, name = "CuentaPerio"),
+    path('EditarCuenta/',EditarCuentaPerio,name="EditarCuentaPerio"),
+
     #-
     path('Login/',LoginUsuario,name="LoginUsuario"), 
     path('AdministrarUsuario/',AdministrarUsuario,name="AdministrarUsuario"), 
     path('BuscarAnalisis/',BuscarAnalisis,name="BuscarAnalisis"),
     path('BuscarUsuario/',BuscarUsuario,name="BuscarUsuario"),
     path('EditarUsuario/',EditarUsuario,name="EditarUsuario"),
-    path('CuentaUsuario/',CuentaUsuario,name="CuentaUsuario"),
-    path('EditarCuenta/',EditarCuenta,name="EditarCuenta"),
-    path('PerfilUsuario/',PerfilUsuario,name="PerfilUsuario"),
+    
+    
+    
     path('PerfilRoman/<int:id>/',PerfilCompleto),
 
 
