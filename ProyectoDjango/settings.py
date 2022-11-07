77fc1d58f377af01dde1133a5e3b57acb854b42c
pablosonn <pa.meliqueo@duocuc.cl>
2022-11-07 18:39:50 -0300

    inicio

--- ProyectoDjango/settings.py
+++ ProyectoDjango/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
         'NAME': '127.0.0.1:1521/xe',
-        'USER': 'AdminGigaChad',
+        'USER': 'AdminGigaChad2',
         'PASSWORD':'Chiste12345',
         'TEST': {
             'USER': 'default_test',
